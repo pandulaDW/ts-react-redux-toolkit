61fa6c10cddd50283821ea8f7493e0ffe0b78c31
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2020-12-20 17:12:10 +0530

    Added typography styles

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -6,10 +6,18 @@ import { increment, decrement, incrementByAmount } from "../redux/counter";
 import styles from "../styles/counter.module.scss";
 
 const Counter = () => {
-  const [incAmount, setIncAmount] = React.useState(1);
-
   const dispatch = useDispatch();
+  const [incAmount, setIncAmount] = React.useState(1);
   const { count } = useSelector((state: RootState) => state.counter);
+
+  const handleIncrement = () => dispatch(increment());
+  const handleDecrement = () => dispatch(decrement());
+  const handleIncrementByAmount = () => dispatch(incrementByAmount(incAmount));
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value);
+    if (isNaN(value)) setIncAmount(0);
+    else setIncAmount(value);
+  };
 
   return (
     <div className={styles.container}>
@@ -21,18 +29,12 @@ const Counter = () => {
           Increment
           <br /> Amount
         </p>
-        <input
-          type="text"
-          value={incAmount}
-          onChange={(e) => setIncAmount(parseInt(e.target.value))}
-        />
+        <input type="text" value={incAmount} onChange={handleInputChange} />
       </div>
       <div className={styles.buttons}>
-        <button onClick={() => dispatch(increment())}>Increment</button>
-        <button onClick={() => dispatch(incrementByAmount(incAmount))}>
-          Increment By Amount
-        </button>
-        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleIncrementByAmount}>Increment By Amount</button>
+        <button onClick={handleDecrement}>Decrement</button>
       </div>
     </div>
   );
