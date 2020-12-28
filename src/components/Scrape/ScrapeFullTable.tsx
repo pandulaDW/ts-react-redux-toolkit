e5a080fce0ae508f691376d4411228670e003876
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2020-12-28 09:44:54 +0530

    Tested css for fixed header

--- src/components/Scrape/ScrapeFullTable.tsx
+++ src/components/Scrape/ScrapeFullTable.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Table } from "evergreen-ui";
 import { ScrapeDataType } from "../../models/scrapeTypes";
 import { RootState } from "../../redux/_store";
 import styles from "../../styles/scrape.module.scss";
@@ -14,39 +13,18 @@ const ScrapeFullTable: React.FC<Props> = ({ data }) => {
 
   return (
     <div className={styles.tableContainer}>
-      <Table>
-        <Table.Head>
-          <Table.TextHeaderCell>KFID</Table.TextHeaderCell>
-          <Table.TextHeaderCell>RA ID</Table.TextHeaderCell>
-          {fieldList.map((field, id) => (
-            <>
-              <Table.TextHeaderCell>{`${field}`}</Table.TextHeaderCell>
-              <Table.TextHeaderCell>{`scraped ${field}`}</Table.TextHeaderCell>
-            </>
-          ))}
-        </Table.Head>
-        <Table.Body>
-          {data.map((item) => (
-            <Table.Row key={item.kfid}>
-              <Table.TextCell>{item.kfid}</Table.TextCell>
-              <Table.TextCell>{item.RAId}</Table.TextCell>
-              {fieldList.map((field) =>
-                field in item ? (
-                  <>
-                    <Table.TextCell>{item[field].uv_value}</Table.TextCell>
-                    <Table.TextCell>{item[field].scraped_value}</Table.TextCell>
-                  </>
-                ) : (
-                  <>
-                    <Table.TextCell />
-                    <Table.TextCell />
-                  </>
-                )
-              )}
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
+      <div className={styles.tableContainer__header}>
+        <div className={styles["tableContainer__header-cell"]}>
+          This is header
+        </div>
+      </div>
+      <div className={styles.tableContainer__body}>
+        <div className={styles["tableContainer__body-section"]}>section 1</div>
+        <div className={styles["tableContainer__body-section"]}>section 2</div>
+        <div className={styles["tableContainer__body-section"]}>section 3</div>
+        <div className={styles["tableContainer__body-section"]}>section 4</div>
+        <div className={styles["tableContainer__body-section"]}>section 5</div>
+      </div>
     </div>
   );
 };
