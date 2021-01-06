c67e82a475b8f4eb234261989ae1c1874c22bc74
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-01-06 18:41:33 +0530

    Added random data return

--- server/handlers/test.js
+++ server/handlers/test.js
@@ -1,13 +1,47 @@
+const path = require("path");
+const fs = require("fs");
+
+const filepath = path.join(__dirname, "..", "data", "requestData.json");
+const allData = JSON.parse(fs.readFileSync(filepath, "utf-8"));
+
+const range = (end, start = 0, step = 1) => {
+  const seq = [];
+  for (let index = start; index < end; index += step) {
+    seq.push(index);
+  }
+  return seq;
+};
+
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+};
+
+const genRandomNums = (limit) => {
+  const seq = range(allData.length);
+  shuffleArray(seq);
+  return seq.slice(0, limit);
+};
+
 exports.testHandler = async (req, res) => {
-  // TODOs
-  // Read data file from request and pick random set of data
+  const requestData = [];
 
-  res.end("Response sent"); // Send back data (timestamp plus kfid)
+  genRandomNums(10).forEach((index) => {
+    requestData.push(allData[index]);
+  });
 
-  let counter = 1;
-  const timeout = setInterval(() => {
-    console.log("Scraping on progress", counter); // Put request (timestamp plus kfid)
-    counter++;
-    if (counter > 10) clearInterval(timeout); // clear interval when all data has been inserted
-  }, 2000);
+  // Sending back data (timestamp plus kfid)
+  res.json({
+    timestamp: new Date().getTime(),
+    kfids: requestData.map((el) => el.kfid),
+  });
+
+  // let counter = 1;
+  // const timeout = setInterval(() => {
+  //   console.log("Scraping on progress", counter); // Put request (timestamp plus kfid)
+  //   counter++;
+  //   if (counter > 10) clearInterval(timeout); // clear interval when all data has been inserted
+  // }, 2000);
 };
