5b0f56e9546591cb175cd339a3d21d43b18b2bc6
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-08-04 12:31:43 +0530

    added sso link to redirect

--- src/components/Auth/RedirectComponent.tsx
+++ src/components/Auth/RedirectComponent.tsx
@@ -1,7 +1,14 @@
+import styles from "../../styles/redirect.module.scss";
+
 const RedirectComponent = () => {
+  const ssoUri = process.env.REACT_APP_CLOUDFRONT_SSO;
+
   return (
-    <div>
-      <h1>You are in the wrong place mate!!!</h1>
+    <div className={styles["redirect"]}>
+      <h1>Please log in to access LEI Productivity Tools Portal</h1>
+      <h3>Contact bsllabs-automations@lseg.com if issue persists</h3>
+
+      <a href={`${ssoUri}`}>Click here to authenticate with SSO</a>
     </div>
   );
 };
